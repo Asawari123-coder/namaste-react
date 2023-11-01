1cf25c7818a9f70d91e3b2eb55f80ffcb4f718dc
Asawari <asawari.km@gmail.com>
2023-11-01 16:03:13 +0530

    Functional Component Basic-- Laying Foundations

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
+
+// React element
 const heading =  React.createElement("h1", { id: "heading" }, "Namaste React!" );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const jsxHeading = <h1 id="heading">Namaste React using JSX!!!!</h1>
+// JSX Element
+const jsxHeading = (
+<h1 id="heading">
+    Namaste React using JSX!!!!
+    </h1>
+    );
 
-root.render(jsxHeading);
+// React Functional Component
+
+const HeadingComponent = () =>{
+    return <h1 id="heading">Namaste React from Functional component </h1>
+}
+
+const HeadingComponent2 = () => (
+<div id="container">
+    {heading}
+    <h1 className="heading"> Namaste React Using Functional Component</h1>
+    <HeadingComponent/>
+    <HeadingComponent></HeadingComponent>
+    {HeadingComponent()}
+</div>
+)
+
+
+root.render(<HeadingComponent2 />);
