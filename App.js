76b8e150e30513e2d2c86e02585831339a427f78
Asawari <asawari.km@gmail.com>
2023-11-02 08:52:27 +0530

    Part01- Static card and app

--- App.js
+++ App.js
@@ -1,34 +1,74 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
+import React from 'react'
 
-// React element
-const heading =  React.createElement("h1", { id: "heading" }, "Namaste React!" );
+import React from 'react'
+
+const styleCard = {
+    backgroundColor: "#f0f0f0",
+}
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="logo-container">
+            <img className="logo" src="https://logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+            <img alt="Res logo" className="res-card-image" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ngmsdg7jvwbbg1rmxze7" /> 
+            <h3>Meghana Foods</h3>
+            <h4>Biryani North Indian Asian </h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {/* Restaurant cards */}
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// JSX Element
-const jsxHeading = (
-<h1 id="heading">
-    Namaste React using JSX!!!!
-    </h1>
-    );
+root.render(<AppLayout />);
 
-// React Functional Component
-
-const HeadingComponent = () =>{
-    return <h1 id="heading">Namaste React from Functional component </h1>
-}
-
-const HeadingComponent2 = () => (
-<div id="container">
-    {heading}
-    <h1 className="heading"> Namaste React Using Functional Component</h1>
-    <HeadingComponent/>
-    <HeadingComponent></HeadingComponent>
-    {HeadingComponent()}
-</div>
-)
-
-
-root.render(<HeadingComponent2 />);
---
+++ Notes/Episode04.js
@@ -0,0 +1,32 @@
+/* 
+Food Ordering APP
+Steps : When you build any project then we have to follow steps as below
+1. Planning
+    a. Header
+        -Logo 
+        -Nav Items (links)
+    b. Body
+        - Search Bar
+        - Card Container
+        - Cards
+            - Image
+            - ResName, star rating,  cuisine, delivery time
+
+    c. Footer
+        - Copyright
+        - Links
+        - Address
+        - Contact
+- Make Reusable component
+- 
+
+
+
+
+
+
+
+
+
+
+*/
