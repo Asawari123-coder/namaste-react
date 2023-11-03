3643a168478bb7670a4e61a295837e88f6cd9413
Asawari <asawari.km@gmail.com>
2023-11-03 18:37:17 +0530

    Part3-Dynamic card with  map function

--- Notes/Episode04.js
+++ Notes/Episode04.js
@@ -31,7 +31,19 @@ React application => UI layer => data layer both makes application
 Best Practices of writing our code
 Code Reviewing code
 - Cleaning mess
-- 
+* Always make habit when you are going to use map or looping anything then must give key.
+why ?
+because when any item added in th list then react did not which one is new one so react will render all
+items. if you provide a key then it renders only new one item.
+using array index as bad practice
+use unique id as key 
+
+Summary
+1. Planning 
+2. Make it static 
+3. Make it dynamic
+4. Use key as unique id as key 
+5. Clean your mess
 
 
 
