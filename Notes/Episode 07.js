d75a614938adac96362a5b21e07cd2bb187deede
Asawari <asawari.km@gmail.com>
2023-11-22 06:24:54 +0530

    Episode07 part 5

--- Notes/Episode 07.js
+++ Notes/Episode 07.js
@@ -15,11 +15,29 @@ It is used for creating local state variable of FC.
 
 
 ## ROUTING ##
+Part3
 1. npm i react-router-dom
 2. Whenever you have to use routes first do routing configuration using createBrowser from react-router-dom
 3. Now we have to render that particular component using RouterProvider
 4. If you entered wrong route or any random text then it shows 404 page not found on web page 
 5. We can create our own error page using errorelement in createBrowser and useRouterError;
+
+part4
+How can we make children routes
+1. First add property children and in that add all paths and use Outlet
+
+How can we Link our routes ?
+There are two approaches 
+1. We can use Anchor tag <a> </a> because when we are doing this way the whole page is got refreshed
+2. Without reloading page we can use Link component
+Single Page application : Component interchange no whole page is not get reloading 
+
+2 types of routing
+1. Client side routing : React using this approach 
+2. Server side routing : makes network call -> get the data -> fetch page -> renders page -> refresh  
+
+
+
 
 
 
